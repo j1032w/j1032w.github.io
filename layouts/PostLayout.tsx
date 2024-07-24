6baac7234552afc1e0d874c5c26fc796ded05223
J1032 <J1032@J1032w.com>
2024-07-23 23:34:19 -0600

    Fixed lint issue

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -56,7 +56,6 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             </div>
           </header>
           <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
-
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
               <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
@@ -128,5 +127,5 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
         </div>
       </article>
     </SectionContainer>
-  );
+  )
 }
