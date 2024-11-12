4038f5b904aa82d8dc4a675c66d6d652ef6c47fa
J1032W <j1032w@users.noreply.github.com>
2024-11-11 21:54:19 -0700

    Updated project info

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,22 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Angular Dashboard Starter',
+    description: `A dashboard starter pack build with Angular 18, Nest.js 9, TypeScript 5`,
+    imgSrc: '/static/images/angular-dashboard-screen.png',
+    href: 'https://www.j1032.com/dashboard',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'React.js Dashboard Starter',
+    description: `ReactJS frontend and NestJS backend dashboard starter, build with React 18,  Nest.js 9, Nx monorepo, TypeScript`,
+    imgSrc: '/static/images/react-dashboard-swagger-screen.png',
+    href: 'https://github.com/j1032w/das-mono-nestjs-reactjs',
   },
 ]
 
