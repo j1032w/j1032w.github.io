c2d2d969edb1a67f06b4a3cc4b864c3ba124d2cd
J1032W <j1032w@users.noreply.github.com>
2024-11-12 00:50:24 -0700

    Fixed lint issue

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,5 @@
-import projectsData from '@/data/projectsData'
 import Card from '@/components/Card'
+import projectsData from '@/data/projectsData'
 import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({ title: 'Projects' })
@@ -12,9 +12,7 @@ export default function Projects() {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Projects
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-
-          </p>
+          {/*<p className="text-lg leading-7 text-gray-500 dark:text-gray-400"></p>*/}
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
